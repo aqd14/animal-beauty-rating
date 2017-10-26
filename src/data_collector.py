d0b4d8481383aba105bf3647e10cf4cb69749893
Anh Quoc Do <aqd14@msstate.edu>
2017-10-26 14:03:45 -0500

    Fix bug config key wrong

--- src/data_collector.py
+++ src/data_collector.py
@@ -10,11 +10,11 @@ from flickrapi import FlickrAPI
 from pprint import pprint
 
 
-config = json.load(open('./flickr.config'))
+config = json.load(open('../flickr.config'))
 
 DATA_DIR = './data'
-API_KEY = config['flickr_api_key']
-API_SECRET = config['flickr_api_secret']
+API_KEY = config['flickr-api-key']
+API_SECRET = config['flickr-api-secret']
 
 def download_image(url, file_name):
     content = requests.get(url, stream=True)
@@ -25,12 +25,13 @@ def download_image(url, file_name):
     else:
         print('Can\'t download image at url: {0}'.format(url))
 
-flickr = FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
-extras='url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
-cats = flickr.photos.search(tags=['cat', 'dogs'], per_page=5, extras=extras)
-url = cats['photos']['photo'][1]['url_s']
-# print(type(photos))
-pprint(url)
-
-if __name__ == 'main':
-    download_image(url, 'test.jpg')        
+def main():
+    flickr = FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
+    extras='url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
+    cats = flickr.photos.search(tags=['dogs'], per_page=10, extras=extras)
+    url = cats['photos']['photo'][1]['url_s']
+    # print(type(photos))
+    pprint(url)
+    download_image(url, 'test.jpg')
+if __name__ == '__main__':
+    main()
